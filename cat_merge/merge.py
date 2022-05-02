46be031f9c6815ce2feb6e45c6db59b2b6b3e521
glass-ships <glass.ships@outlook.com>
2022-05-02 16:41:46 -0600

    allow dir or list of files as input

--- cat_merge/merge.py
+++ cat_merge/merge.py
@@ -1,19 +1,36 @@
-import typer
 from typing import List
-from file_utils import write, read_dfs
-from merge_utils import merge_kg
+from cat_merge.file_utils import *
+from cat_merge.merge_utils import *
 
+import typer
+typer_app = typer.Typer()
 
-def merge(name: str,
-          edges: List[str] = typer.Option(...),
-          nodes: List[str] = typer.Option(...),
-          output_dir: str = typer.Option("merge-output")):
-    node_dfs = read_dfs(nodes)
-    edge_dfs = read_dfs(edges)
-    write(name=name,
-          kg=merge_kg(node_dfs=node_dfs, edge_dfs=edge_dfs),
-          output_dir=output_dir)
+#@typer_app.command()
+def merge(
+    name: str = "merged-kg",#typer.Option("merged-kg", help="Name of the resulting knowledge graph"),
+    input_dir: str = None,#typer.Option(None, help="Optional directory containing node and edge files"),
+    edges: List[str] = None,#typer.Option(None, help="Optional list of edge files"),
+    nodes: List[str] = None,#typer.Option(None, help="Optional list of node files"),
+    output_dir: str = "merged-output",#typer.Option("merge-output", help="Directory to output knowledge graph")
+    ):
+
+    print(f"Name: {name} // input_dir: {input_dir} // nodes: {nodes} // edges: {edges} // output_dir: {output_dir}")
+
+    if nodes is not None and edges is not None:
+        node_dfs = read_dfs(nodes)
+        edge_dfs = read_dfs(edges)
+    elif input_dir is not None:
+        node_files, edge_files = get_files(input_dir)
+        node_dfs = read_dfs(node_files)
+        edge_dfs = read_dfs(edge_files)
+
+    write(
+        name=name,
+        kg=merge_kg(node_dfs=node_dfs, edge_dfs=edge_dfs),
+        output_dir=output_dir
+    )
 
 
 if __name__ == "__main__":
-    typer.run(merge)
+    #typer.run(merge)
+    typer_app()

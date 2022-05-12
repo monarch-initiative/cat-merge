456b96821c1ca3dfc9da94c1458758c8d1cbbea6
caufieldjh <j.harry.caufield@gmail.com>
2022-05-12 17:22:16 -0400

    Add option to modify merge delimiter for dup nodes

--- cat_merge/merge_utils.py
+++ cat_merge/merge_utils.py
@@ -13,12 +13,12 @@ def get_duplicate_rows(df: DataFrame) -> DataFrame:
     return df[df.index.duplicated(keep=False)]
 
 
-def clean_nodes(nodes: DataFrame) -> DataFrame:
+def clean_nodes(nodes: DataFrame, merge_delimiter: str) -> DataFrame:
     nodes.reset_index(inplace=True)
     nodes.drop_duplicates(inplace=True)
     nodes = nodes.rename(columns={'index': 'id'})
     nodes.fillna("None", inplace=True)
-    column_agg = {x: ' '.join for x in nodes.columns if x != 'id'}
+    column_agg = {x: merge_delimiter.join for x in nodes.columns if x != 'id'}
     nodes = nodes.groupby(['id'], as_index=True).agg(column_agg)
     return nodes
 
@@ -31,7 +31,7 @@ def get_dangling_edges(edges: DataFrame, nodes: DataFrame) -> DataFrame:
     return edges[~edges.subject.isin(nodes.index) | ~edges.object.isin(nodes.index)]
 
 
-def merge_kg(edge_dfs: List[DataFrame], node_dfs: List[DataFrame]) -> MergedKG:
+def merge_kg(edge_dfs: List[DataFrame], node_dfs: List[DataFrame], merge_delimiter: str) -> MergedKG:
 
     all_nodes = concat_dataframes(node_dfs)
     all_edges = concat_dataframes(edge_dfs)
@@ -39,7 +39,7 @@ def merge_kg(edge_dfs: List[DataFrame], node_dfs: List[DataFrame]) -> MergedKG:
     duplicate_nodes = get_duplicate_rows(df=all_nodes)
     dangling_edges = get_dangling_edges(edges=all_edges, nodes=all_nodes)
 
-    nodes = clean_nodes(nodes=all_nodes)
+    nodes = clean_nodes(nodes=all_nodes, merge_delimiter=merge_delimiter)
     edges = clean_edges(edges=all_edges, nodes=nodes)
 
     return MergedKG(nodes=nodes, edges=edges, duplicate_nodes=duplicate_nodes, dangling_edges=dangling_edges)

e93cacdb185a257f4ba12fc0f2d285cdbac1d229
Kevin Schaper <kevinschaper@gmail.com>
2022-05-12 15:03:54 -0700

    added pandas logic to apply mappings

--- cat_merge/mapping_utils.py
+++ cat_merge/mapping_utils.py
@@ -1,10 +1,17 @@
-import pandas as pd
+import numpy as np
 from pandas.core.frame import DataFrame
-from typing import List
 
 
 def apply_mappings(edges: DataFrame, mapping: DataFrame):
 
-    # todo: apply mappings
+    mapping_dict = mapping.set_index('subject_id')['object_id']
+
+    edges['original_subject'] = edges['subject']
+    edges['subject'].replace(mapping_dict, inplace=True)
+    edges['original_subject'] = np.where(edges.subject == edges.original_subject, None, edges.original_subject)
+
+    edges['original_object'] = edges['object']
+    edges['object'].replace(mapping_dict, inplace=True)
+    edges['original_object'] = np.where(edges.object == edges.original_object, None, edges.original_object)
 
     return edges

869f3a2a1153188ddc353b43740b6a37b5f97e6b
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2022-09-07 10:08:57 -0500

    Fix namespace to handle series, test was incorrect

--- tests/unit/test_get_namespace.py
+++ tests/unit/test_get_namespace.py
@@ -1,12 +1,16 @@
 import pytest
 from cat_merge.qc_utils import get_namespace
+import pandas as pd
 
 
 @pytest.fixture
-def id_str() -> str:
-    return 'TEST:TEST-GENE-010000-1'
+def series1() -> pd.Series:
+    S = pd.Series(['TEST:TEST-GENE-010000-1', 'TEST2:TEST-GENE-010000-2', 'TEST3:TEST-GENE-010002-1'])
+    return S
 
 
-def test_get_namespace(id_str):
-    test_namespace = get_namespace(id_str)
-    assert (test_namespace == 'TEST')
+def test_get_namespace_length(series1):
+    test_namespace = get_namespace(series1)
+    assert (len(test_namespace) == 3)
+
+

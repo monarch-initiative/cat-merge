75bbd147c029a38b5b551e25858932afb02cb68a
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2022-09-21 14:21:39 -0500

    Remove top level functions in qc.py from qc_diff_utils.py

--- cat_merge/qc_diff_utils.py
+++ cat_merge/qc_diff_utils.py
@@ -1,23 +1,5 @@
 import yaml
-from cat_merge.file_utils import read_kg
-from cat_merge.qc_utils import create_qc_report
-
 from typing import Dict, List, Union
-
-
-def qc_report(archive_path: str,
-              output_dir: str,
-              output_name: str = "qc_report.yaml",
-              add_provided_by: bool = True,
-              # dangling_edges: bool = True,
-              # dangling_edges_path: str = None,
-              nodes_file_name: str = None,
-              edges_file_name: str = None):
-    kg = read_kg(archive_path)
-    report = create_qc_report(kg)
-
-    with open(f"{output_dir}/{output_name}", "w") as report_file:
-        yaml.dump(report, report_file)
 
 
 def print_types(a: Union[Dict, List, str]):
@@ -175,22 +157,9 @@ def get_source_names(sources: Union[List[Dict], None]) -> Dict:
     return names
 
 
-def compare_yaml(a_yaml: Dict, b_yaml: Dict) -> Dict:
+def diff_yaml(a_yaml: Dict, b_yaml: Dict) -> Dict:
     yaml_qc_compare = {}
     for key in dict.fromkeys(list(a_yaml.keys()) + list(b_yaml.keys())):
         yaml_qc_compare[key] = compare_nodes_qc(a_yaml.get(key), b_yaml.get(key))
 
     return yaml_qc_compare
-
-
-def qc_compare(qc_file_a: str, qc_file_b: str):
-    # with open(qc_file_a, "r") as yml_file:
-    #     qc_yaml_a = load(yml_file, Loader=get_qc_yaml_loader())
-    with open(qc_file_a, "r") as yml_file:
-        qc_yaml_a = yaml.safe_load(yml_file)
-    with open(qc_file_b, "r") as yml_file:
-        qc_yaml_b = yaml.safe_load(yml_file)
-
-    qc_compare_report = compare_yaml(qc_yaml_a, qc_yaml_b)
-    return qc_compare_report
-

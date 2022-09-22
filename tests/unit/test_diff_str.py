c5317bd8fbd3e7566514dfb9160b690a6e6207ec
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2022-09-22 15:07:01 -0500

    Add tests for diff_str

---
+++ tests/unit/test_diff_str.py
@@ -0,0 +1,28 @@
+import pytest
+from cat_merge.qc_diff_utils import diff_str
+
+
+@pytest.fixture
+def str1() -> str:
+    return "source1.tsv"
+
+
+def test_diff_str_match(str1):
+    assert (diff_str(str1, str1) == "source1.tsv")
+
+
+def test_diff_str_a_none(str1):
+    assert (diff_str(None, str1) == "-source1.tsv")
+
+
+def test_diff_str_b_none(str1):
+    assert (diff_str(str1, None) == "+source1.tsv")
+
+
+@pytest.fixture
+def str2() -> str:
+    return "source2.tsv"
+
+
+def test_diff_int_no_match(str1, str2):
+    assert (diff_str(str1, str2) == ["+source1.tsv", "-source2.tsv"])

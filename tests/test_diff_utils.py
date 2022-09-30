bff00f4b9b01a3780b7079e686b90244605e894b
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2022-09-30 12:11:29 -0500

    Create test factory for testing diff flags  - Created test_diff_utils.py for test function factory  - Fixed test_diff_int.py

---
+++ tests/test_diff_utils.py
@@ -0,0 +1,10 @@
+flags_params = {"change_T_show_T": {"change": True, "show_all": True},
+                "change_F_show_T": {"change": False, "show_all": True},
+                "change_T_show_F": {"change": True, "show_all": False},
+                "change_F_show_F": {"change": False, "show_all": False}}
+
+
+def pytest_generate_tests(metafunc):
+    if "flags" in metafunc.fixturenames:
+        # metafunc.parametrize("flags", flags_params, ids=flags_params_ids)
+        metafunc.parametrize("flags", flags_params.values(), ids=list(flags_params.keys()))

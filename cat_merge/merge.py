7f5a17e307872915f6ebb1bf3d7be829755fbd41
Kevin Schaper <kevinschaper@gmail.com>
2023-08-18 13:21:01 -0700

    simplify check for passing in source vs nodes & edges

--- cat_merge/merge.py
+++ cat_merge/merge.py
@@ -45,7 +45,7 @@ Merging KG files...
   mappings: {mappings}
   output_dir: {output_dir}
 """)
-    if nodes is None != (edges is None) or source is None == (nodes is None):
+    if source is None and (nodes is None or edges is None):
         raise ValueError("Wrong attributes: must specify both nodes & edges or source")
 
     if source is not None and (nodes or edges):
